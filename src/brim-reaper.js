cd05d6223f96c915100eaf9d736993b67770b257
AaronLobitana <faaron.lobitana@gmail.com>
2023-02-09 23:26:28 -0500

    brim-reaperV1.0.0
    Ported card HTML and CSS w/out JS button functionality.

--- src/brim-reaper.js
+++ src/brim-reaper.js
@@ -1,6 +1,6 @@
 import { LitElement, html, css } from 'lit';
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+const logo = new URL('../assets/brim-reaper2.PNG', import.meta.url).href;
 
 class BrimReaper extends LitElement {
   static properties = {
@@ -8,7 +8,8 @@ class BrimReaper extends LitElement {
   }
 
   static styles = css`
-    :host {
+    :host 
+    {
       min-height: 100vh;
       display: flex;
       flex-direction: column;
@@ -48,28 +49,216 @@ class BrimReaper extends LitElement {
     .app-footer a {
       margin-left: 5px;
     }
+
+    .myimage{
+      width: 450px;
+    }
+
+    .overallcard{
+      width: 400px;
+      border: 4px solid black;
+      display: inline-flex;
+    }
+
+    .box{
+      background-color: #B19CD9;
+    }
+
+    .pic{
+      width: 400px;
+      transition: transform .5s ease-in-out;
+    }
+
+    .pic:hover{
+      transform: rotate(360deg);
+    }
+
+    .heading{
+      text-align: center;
+    }
+
+    .details{
+      text-align: center;
+      display: none;
+    }
+
+    .topbuttons{
+      margin: 10px 0px 10px 0px;
+      display: block;
+    }
+
+
+    .dupe{
+      background-color: #B19CD9;
+      color: white;
+      text-align: center;
+      padding: 15px;
+      transition: background-color 0.8s;
+      border: 4px double white;
+      font-size: 20px;
+    }
+
+    .changebg{
+      background-color: #B19CD9;
+      color: white;
+      text-align: center;
+      padding: 15px;
+      transition: background-color 0.8s;
+      border: 4px double white;
+      font-size: 20px;
+    }
+
+    .changehg{
+      background-color: #B19CD9;
+      color: white;
+      text-align: center;
+      padding: 15px;
+      transition: background-color 0.8s;
+      border: 4px double white;
+      font-size: 20px;
+    }
+
+    .del{
+      background-color: #B19CD9;
+      color: white;
+      text-align: center;
+      padding: 15px;
+      transition: background-color 0.8s;
+      border: 4px double white;
+      font-size: 20px;
+    }
+
+    .detailsbutton{
+      background-color: #B19CD9;
+      color: white;
+      text-align: center;
+      padding: 15px;
+      transition: background-color 0.8s;
+      border: 4px double white;
+      font-size: 20px;
+      margin: 10px;
+    }
+
+    .detailsbutton:hover{
+      background-color: black;
+    }
+
+    .detailsbutton:focus{
+      background: gray;
+      color: #041E42;
+    }
+
+    .reveal{
+      text-align: center;
+    }
+
+
+    .dupe:hover{
+      background-color: black;
+    }
+
+    .dupe:focus{
+      background: gray;
+      color: #041E42;
+    }
+
+    .changebg:hover{
+      background-color: black;
+    }
+
+    .changebg:focus{
+      background: gray;
+      color: #041E42;
+    }
+
+    .changehg:hover{
+      background-color: black;
+    }
+
+    .changehg:focus{
+      background: gray;
+      color: #041E42;
+    }
+
+    .del:hover{
+      background-color: black;
+    }
+
+    .del:focus{
+      background: gray;
+      color: #041E42;
+    }
+
+    @media only screen and (max-width: 425px)
+    {
+      .reveal{
+        display: none;
+      }
+
+      .overallcard{
+        width: 250px;
+        border: 4px solid black;
+        display: inline-flex;
+      }
+      
+      .pic{
+        width: 250px;
+        transition: transform .5s ease-in-out;
+      }
+  }
+
   `;
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.header = 'The Brim Reaper';
   }
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
+        <div class="logo"><img class="myimage" alt="open-wc logo" src=${logo} /></div>
         <h1>${this.header}</h1>
 
-        <p>Edit <code>src/BrimReaper.js</code> and save to reload.</p>
+        <div class="topbuttons">
+          <button class="dupe">Duplicate</button>
+          <button class="changebg">Change Background</button>
+          <button class="changehg">Change Heading</button>
+          <button class="del" id="del">Delete</button>
+        </div>
+
+        <div class="overallcard">
+          <div class="box">
+    
+            <img class="pic" src="https://pbs.twimg.com/media/FajpQj_XwAEwCWw.jpg" alt="the brim reaper">
+    
+            <div class="heading">
+              <h1>the brim reaper</h1>
+            </div>
+    
+            <div class="details">
+              <h3>fear the reaper</h3>
+            </div>
+            <div class="reveal">
+              <button class="detailsbutton">Details</button>
+            </div>
+          </div>
+        </div>
+
+    
+
+
+        <p>Disclaimer: This is a fan page. Image credits go to </p>
         <a
           class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
+          href="https://www.instagram.com/williamknightt/?hl=en"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Code examples
+          William Knight
         </a>
+
+
       </main>
 
       <p class="app-footer">
@@ -78,7 +267,7 @@ class BrimReaper extends LitElement {
           target="_blank"
           rel="noopener noreferrer"
           href="https://github.com/open-wc"
-          >open-wc</a
+          >Aaron lobitana</a
         >.
       </p>
     `;
